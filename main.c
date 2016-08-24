62aae6f3f354c265f4e98771fca62747bdf77aef
Jesús Martín <a01227717@itesm.mx>
2016-08-23 21:33:19 -0500

    un led interrupciones timer

--- main.c
+++ main.c
@@ -1,40 +1,33 @@
 #include <hidef.h> /* for EnableInterrupts macro */
 #include "derivative.h" /* include peripheral declarations */
 
-unsigned char arreglo[] = {0x0E, 0x0D, 0x0B, 0x07}; //char para 8 bits
-unsigned char j;
 
-interrupt 23 void RTI_ISR(void){
-		
-		PTBD = arreglo[j];
-		if(PTAD_PTAD2 == 0){
-			j++;
-			if(j==4)
-				j=0;
-		}else{
-			j--;
-			if(j==255) //le dimos la vuelta al rango de 0 a 255 de los 8 bits de j
-				j=3;
-			
-		}
-		SRTISC_RTIACK=1;
+
+
+interrupt 12 void MTIM(void){
+	(void)MTIMSC; //cast al vacio,
+	MTIMSC_TOF=0;//apagar bandera
+	PTBD_PTBD7=!PTBD_PTBD7;
+	
 }
 
 
 
 
 void main(void) {
+	EnableInterrupts;
   /* include your code here */
-  PTBDD =  0x0F; //del 0 al 3 son salidas, y del 4 al 7 son entradas
-  PTAPE_PTAPE2 = 1;
-  SRTISC=0b00010110;
-  j=0;
- 
+	MTIMSC_TOIE=1;//habilitador local
+  PTBDD_PTBDD7=1; //Salida del led de la tarjeta
+  MTIMCLK=0b00000100;
+  MTIMSC_TSTP=0;
+  MTIMMOD=250;// comparador
+  
+
   for(;;) {
 	  
 	  __RESET_WATCHDOG();
 		
-		
-
+	  
   } /* loop forever */
 }
